6d662233b8f7dd4b33a19bb23b960b73ae4aafe8
Christian Engel <christian.engel@wearekiss.com>
2016-11-11 16:54:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+SmartImages
+===========
+
+This javascript library makes the <img> tags in your website smarter. It enables you to define different source URLs
+for desktop and mobile devices and even differentiate between regular and retina displays.
+
+SmartImages also brings a basic lazy loading functionality that helps reducing the initial load impact of your website.
+
+
+##How it works
+To make use of the smart loading behaviour, you need to leave the `src` property of your image empty. This way you
+are preventing the browser from directly starting to download the image contents upon pageload.
+
+You can define up to four image sources on your image tag like so:
+
+````html
+<img
+	data-src="regularDesktopImage.jpg"
+	data-src-2x="retinaDesktopImage.jpg"
+	data-src-mobile="regularMobileImage.jpg"
+	data-src-mobile-2x="retinaMobileImage.jpg"
+	>
+````
+
+Note how the `src` property is completely omitted here so the browser won't download anything by himself.
+
+You can simply include the SmartImages library at the bottom of the page and forget about it. You don't need anymore
+   configuration:
+   
+````html
+<script type="text/html" src="smartImages.min.js"></script>
+````
+
+The library will check the browser and then apply the needed image sources for the current environment.
+
+It decides what to use based on two media queries. A device is detected as a retina device, when
+the minimal device pixel ratio is 1.25 or the resolution is more then 120 dpi.
+
+If a device is considered "mobile" will be detected by the media query `(max-width: 650px)`. This can be changed
+manually, if desired.
+
+Heads up: SmartImages will dynamically swap image sources when you resize the browser window
+and cross the border defined in the media query.
+
+
+##Using the lazy loader
+Sometimes you want to reduce the initial loading impact of your website - especially when
+ its using a LOT of image data. You can do so by applying the attribute `data-lazy` to your
+ images. When the attribute is present, SmartImages will only start to download the images
+ when they get near the viewport of the browser. Rule of thumb: everything below
+ scrollPos + (screenHeight * 1.5) is not downloaded yet.
+  
+##Intercepting initialization
+By default, SmartImages listens to the body.load event and will initialize when everything
+else on your page has been loaded. You can disable that behaviour.
+
+When you call `smartImages.noAutoInit()` right after you embedded the library in your
+page, SmartImages won't initialize itself. You need to call `smartImages.init()` manually.
+
+I've implemented this because I had pages where I definitely only wanted to load ONE big
+image above the fold first and nothing else.
+
+This can be done by including the library, disabling the autoInit and then applying
+the SmartImage functionality manually on just one image. After that image has been loaded,
+initialize the library as usual.
+
+Here's an example:
+
+````html
+<script type="text/html" src="smartImages.min.js"></script>
+<script>
+	smartImages.noAutoInit();
+</script>
+
+<img
+	data-src="myHeaderImage.jpg"
+	data-src-2x="myHeaderImage_2x.jpg"
+	data-src-mobile="myHeaderImageMobile.jpg"
+	data-src-mobile-2x="myHeaderImageMobile_2x.jpg"
+	onload="smartImages.init"
+	id="myHeaderImage"
+	>
+	
+<script>
+	smartImages.manualAssign(document.getElementById('myHeaderImage'));
+</script>
+````
+
+Notice how I applied the `init()` method to the `onload` event of the image.
---
+++ smartImages.js
@@ -0,0 +1,125 @@
+/**
+ * SmartImages
+ * ===========
+ * SmartImages provides multiple functions:
+ *
+ * - Loading retina images when necessary and/or possible
+ * - Making image sources responsive by swapping image sources on a predefined breakpoint.
+ * - Adding lazy loading to images to only load when they come near the viewport.
+ *
+ * @author: Christian Engel <hello@wearekiss.com>
+ * @version: 2 (10.11.2016)
+ */
+(function () {
+    'use strict';
+
+    var isRetina,
+        isMobile,
+        smartImages,
+        scrollTimeout,
+        autoInit = true,
+        scrollListening = false,
+        hasLazyImages = false;
+
+    isRetina = window.matchMedia('(-webkit-min-device-pixel-ratio: 1.25),(min-resolution: 120dpi)').matches;
+
+    smartImages = window.smartImages = window.smartImages || {};
+    smartImages.mediaQuery = smartImages.mediaQuery || '(max-width: 650px)';
+
+    function responsiveHandler(e) {
+        isMobile = e.matches;
+        processImages();
+    }
+
+    function assign(img, scrollTop, lazyBorder) {
+        var o = {
+            src1x: img.getAttribute('data-src'),
+            src2x: img.getAttribute('data-src-2x'),
+            src1x_mobile: img.getAttribute('data-src-mobile'),
+            src2x_mobile: img.getAttribute('data-src-mobile-2x'),
+            lazy: img.getAttribute('data-lazy') !== null
+        };
+
+        var src = o.src1x;
+
+        if (isMobile) {
+            if (o.src1x_mobile !== null) {
+                src = o.src1x_mobile;
+            }
+
+            if (isRetina) {
+                if (o.src2x_mobile !== null) {
+                    src = o.src2x_mobile;
+                } else {
+                    if (o.src2x !== null) {
+                        src = o.src2x;
+                    }
+                }
+            }
+        } else {
+            if (isRetina && o.src2x !== null) {
+                src = o.src2x;
+            }
+        }
+
+        if (o.lazy) {
+            hasLazyImages = true;
+
+            if (scrollTop + img.getBoundingClientRect().top > lazyBorder) {
+                return;
+            }
+        }
+
+        if (src === null) {
+            return;
+        }
+
+        img.src = src;
+    }
+
+    function processImages() {
+        var scrollTop, lazyBorder;
+
+        scrollTop = document.body.scrollTop;
+        lazyBorder = scrollTop + (window.innerHeight * 1.5);
+
+        for (var i = 0; i < document.images.length; i++) {
+            assign(document.images[i], scrollTop, lazyBorder);
+        }
+
+        if (hasLazyImages && !scrollListening) {
+            scrollListening = true;
+            window.addEventListener('scroll', function () {
+                clearTimeout(scrollTimeout);
+                scrollTimeout = setTimeout(processImages, 100);
+            });
+        }
+    }
+
+    smartImages.init = function () {
+        //Process all images for the first time.
+        processImages();
+
+        //React on the mobile media query and attach event listeners.
+        var responsiveQuery = window.matchMedia(smartImages.mediaQuery);
+        responsiveHandler(responsiveQuery);
+        responsiveQuery.addListener(responsiveHandler);
+
+        console.log('Images are smart, now.');
+    };
+
+    smartImages.noAutoInit = function () {
+        autoInit = false;
+    };
+
+    smartImages.manualAssign = function(elm){
+        assign(elm, 0, 0);
+    };
+
+    window.addEventListener('load', function(){
+        if(autoInit === true){
+            smartImages.init();
+        }
+    });
+})();
+
